28d0a9188c206b3b942e078d35a626baa182029d
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-04-03 03:45:10 +0300

    lab8

--- server.js
+++ server.js
@@ -10,28 +10,56 @@ const superagent = require('superagent');
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const PARKS_API_KEY = process.env.PARKS_API_KEY;
+const DATABASE_URL= process.env.DATABASE_URL;
+
 let lat='';
 let lon='';
 app.use(cors());
 app.get('/location',handleLocation);
 app.get('/weather',handleWeather);
 app.get('/parks',handlePark);
-app.listen(PORT,()=>{
-    console.log(`this app is listening to the port ${PORT}`);
-});
+// app.listen(PORT,()=>{
+//     console.log(`this app is listening to the port ${PORT}`);
+// });
+const pg = require('pg');
+const client = new pg.Client(DATABASE_URL);
+client.on('error', err => { console.log('Unable to connect to database'); });
 
 
-function handleLocation(req,res){
-const  locationCity = req.query.city;
-let url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&format=json&q=${locationCity}`;
-superagent.get(url).then(dataResponse =>{
-    const data=dataResponse.body[0];
-    // let cityLocation = 
-    lat = data.lat;
-   lon = data.lon;  
-    res.send(new Location(locationCity,data.display_name , data.lat ,data.lon));
-    console.log(data);
-});
+function handleLocation(request,response){
+const  locationCity = request.query.city;
+const dbsql = "SELECT * FROM locations WHERE search_query=$1"
+ let values = [locationCity]
+ client.query(dbsql, values).then((data)=>{
+    if(data.rowCount === 0){
+      let url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&format=json&q=${locationCity}`;
+      superagent.get(url).then(res => {
+        let data = res.body[0];
+        lat = data.lat;
+        lon = data.lon;
+        let locationObject = new Location(locationCity, data.display_name,data.lat,data.lon);
+        const insertValue = 'INSERT INTO locations (search_query,formatted_query, latitude,longitude) VALUES ($1, $2 ,$3 ,$4);';
+        const newRow = [locationObject.search_query, locationObject.formatted_query ,locationObject.latitude,locationObject.longitude];
+        client.query(insertValue, newRow)
+        .then((data) => {
+          response.send(data.rows[0]);
+        });
+      })
+    } else {
+      lat = data.rows[0].latitude;
+      lon = data.rows[0].longitude;
+      response.send(data.rows[0]);
+    }
+  })
+// let url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&format=json&q=${locationCity}`;
+// superagent.get(url).then(dataResponse =>{
+//     const data=dataResponse.body[0];
+   
+//     lat = data.lat;
+//    lon = data.lon;  
+//     res.send(new Location(locationCity,data.display_name , data.lat ,data.lon));
+//     // console.log(data);
+// });
 
 }
 
@@ -100,3 +128,11 @@ function Parks (name,address ,fee,description,url){
     this.url=url
 
 }
+ 
+client.connect().then(() => {
+    app.listen(PORT, () => {
+      console.log('app is listning on port' + PORT);
+    });
+  }).catch(err => {
+    console.log('Sorry there is an error' + err);
+  });
